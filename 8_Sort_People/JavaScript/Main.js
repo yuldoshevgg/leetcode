aaf70d0a004c5434b0170dac636a56ca0f2378ff
yuldoshevgg <devinfinitum0@gmail.com>
2023-06-27 12:39:57 +0500

    Sort_The_People problem.2418

--- 8_Sort_People/JavaScript/Main.js
+++ 8_Sort_People/JavaScript/Main.js
@@ -1,14 +1,22 @@
 var sortPeople = function (names, heights) {
+  let tmp = []
   let newNames = []
-  
+
   for (let i = 0; i < heights.length; i++) {
-    let max = Math.max(...heights)
-    let maxIndex = heights.indexOf(max)
-    heights.splice(maxIndex, 1, -1)
-    newNames.push(names[maxIndex])
+    let obj = {
+      name: names[i],
+      height: heights[i]
+    }
+
+    tmp.push(obj)
   }
-  
+
+  tmp.sort((a, b) => b.height - a.height)
+  for (let i = 0; i < tmp.length; i++) {
+    newNames.push(tmp[i].name)
+  }
+
   return newNames
 };
 
-console.log(sortPeople(["Alice","Bob","Bob"], [155, 185, 150]));
+console.log(sortPeople(["Mary", "John", "Emma"], [180, 165, 170]));
